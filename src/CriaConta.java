062166aa6bfec40f9a6318bab0a9102026d04904
renan-eng <renan3838@gmail.com>
2022-12-14 14:08:46 -0300

    utilizando metodos deposita e pegasaldo

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -1,16 +1,16 @@
 public class CriaConta {
     public static void main(String[] args) {
         Conta primeiraConta = new Conta();
-        primeiraConta.saldo = 200;
-        System.out.println(primeiraConta.saldo);
-        primeiraConta.saldo += 100;
-        System.out.println(primeiraConta.saldo);
+        primeiraConta.deposita(200);
+        System.out.println(primeiraConta.pegaSaldo());
+        primeiraConta.deposita(100);
+        System.out.println(primeiraConta.pegaSaldo());
 
         Conta segundaConta = new Conta();
-        segundaConta.saldo = 50;
+        segundaConta.deposita(50);
 
-        System.out.println("Primeira conta tem: " + primeiraConta.saldo);
-        System.out.println("Segunda conta tem: " + segundaConta.saldo);
+        System.out.println("Primeira conta tem: " + primeiraConta.pegaSaldo());
+        System.out.println("Segunda conta tem: " + segundaConta.pegaSaldo());
         System.out.println("Agencia: " + primeiraConta.agencia);
         System.out.println("Número: " + primeiraConta.numero);
         System.out.println("Titular: " + primeiraConta.titular);
