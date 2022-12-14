76877fd2a95b5aa13a4e0898ec7fb0f0418ed141
renan-eng <renan3838@gmail.com>
2022-12-14 10:27:18 -0300

    Testando metodo de transferencia

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -1,12 +1,21 @@
 public class TestaMetodo {
     public static void main(String[] args) {
-        Conta conta = new Conta();
-        conta.saldo = 200;
-        conta.deposita(50);
-        System.out.println(conta.saldo);
+        Conta contaMaria = new Conta();
+        contaMaria.saldo = 200;
+        contaMaria.deposita(50);
+        System.out.println(contaMaria.saldo);
 
-        boolean saqueRetorno = conta.saca(260);
-        System.out.println(conta.saldo);
+        boolean saqueRetorno = contaMaria.saca(260);
+        System.out.println(contaMaria.saldo);
         System.out.println(saqueRetorno);
+
+        Conta contaJoao = new Conta();
+        contaJoao.deposita(120);
+//        contaJoao.transfere(20, contaMaria);
+        boolean transfereSucesso = contaJoao.transfere(20, contaMaria);
+        System.out.println("Saldo da conta Joao: " + contaJoao.saldo);
+        System.out.println("Saldo da conta Maria: " + contaMaria.saldo);
+        System.out.println(transfereSucesso);
+
     }
 }
